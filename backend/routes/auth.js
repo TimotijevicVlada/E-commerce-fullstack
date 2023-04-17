a6e4599031dac6f551acf793d78b41844fbbb0e0
Vladimir Timotijevic <timotijevicvlada@gmail.com>
2023-04-17 10:50:33 +0200

    Register and login done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,45 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJs = require("crypto-js");
 
 //REGISTER
 router.post("/register", async (req, res) => {
     try {
         const user = new User({
-            ...req.body
+            ...req.body,
+            password: CryptoJs.AES.encrypt(req.body.password, process.env.PASS_SECRET).toString()
         })
         const newUser = await user.save();
-        res.status(200).json(newUser);
+        const { password, ...others } = newUser._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        let errorMessage = "";
+        if (error.keyValue.email) {
+            errorMessage = "This email already exist"
+        } else if (error.keyValue.username) {
+            errorMessage = "This username already exist"
+        } else {
+            errorMessage = error
+        }
+        res.status(500).json(errorMessage);
+    }
+})
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (!user) {
+            res.status(401).json("Wrong username");
+            return;
+        }
+        const hashedPassword = CryptoJs.AES.decrypt(user.password, process.env.PASS_SECRET).toString(CryptoJs.enc.Utf8);
+        if (hashedPassword !== req.body.password) {
+            res.status(401).json("Wrong password");
+            return;
+        }
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
     } catch (error) {
         res.status(500).json(error);
     }
